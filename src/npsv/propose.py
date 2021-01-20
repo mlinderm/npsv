f702c06791bdf61ed30141a2ee13a163ae1da6e1
Michael Linderman <mlinderman@middlebury.edu>
2021-01-20 16:34:38 -0500

    Fix bounds for proposed SVs and introduce blocked selection

--- src/npsv/propose.py
+++ src/npsv/propose.py
@@ -1,10 +1,11 @@
-import collections, logging, sys
+import collections, logging, operator, sys
 import numpy as np
 from scipy.signal import peak_prominences, find_peaks
 from scipy.special import logsumexp
 from scipy.stats import binom, chi2
 import vcf
 import pysam
+from intervaltree import Interval, IntervalTree
 from .variant import Variant, variant_descriptor
 from .genotyper import MAHAL_FEATURES
 
@@ -91,9 +92,9 @@ def propose_variants(args, input_vcf: str, output_file):
             if event_repeat_count == 0:
                 continue
 
-            # Find event starts
+            # Find event starts (don't allow event to extend past end of repeat)
             repeat_start = int(repeat[1]) - PEAK_FINDING_FLANK  # Flank for peak finding
-            repeat_end = int(repeat[2]) + PEAK_FINDING_FLANK
+            repeat_end = int(repeat[2]) + PEAK_FINDING_FLANK - variant.event_length
 
             # repeat is BED-file with half-open start
             ref_seq = variant.reference_sequence(f"{repeat[0]}:{repeat_start+1}-{repeat_end}")
@@ -145,10 +146,12 @@ def propose_variants(args, input_vcf: str, output_file):
             record += "\t."  # Define unknown genotypes for proposed variants
         print(record, file=output_file)
 
+
 def dm2_to_prob(score, df=len(MAHAL_FEATURES)):
     with np.errstate(under="ignore"):
         prob = chi2.logsf(score, df)
         return np.exp(prob - logsumexp(prob))
+
 
 def ad_to_prob(ad):
     ad = [int(reads) for reads in ad]
@@ -159,6 +162,23 @@ def ad_to_prob(ad):
         binom.pmf(ad[1], total_reads, 0.5),
         binom.pmf(ad[1], total_reads, 0.95),
     ]
+
+
+class RangeTree:
+    def __init__(self):
+        self._trees = collections.defaultdict(IntervalTree)
+
+    def add(self, contig: str, start: int, end: int, data):
+        return self._trees[contig].addi(start, end, data)
+
+    def merge_overlaps(self, **kwargs):
+        for tree in self._trees.values():
+            tree.merge_overlaps(**kwargs)
+
+    def values(self):
+        for tree in self._trees.values():
+            for _, _, data in tree.items():
+                yield data
 
 
 def refine_variants(args, input_vcf: str, output_file):
@@ -173,8 +193,12 @@ def refine_variants(args, input_vcf: str, output_file):
         output_file: Output file object
     """
 
+    orig_start_idx = 0 if args.include_orig_ref else 1
+
     # Setup VCF reader and writer...
     vcf_reader = vcf.Reader(filename=input_vcf)
+    num_samples = len(vcf_reader.samples)
+
     # Add new format fields
     vcf_reader.formats["CL"] = vcf.parser._Format(
         "CL", "1", "String", "Call location used for genotype",
@@ -213,24 +237,73 @@ def refine_variants(args, input_vcf: str, output_file):
                 else:
                     alternate_records[original] = [record]
 
+    # Determine variant groups 
+    variant_ranges = RangeTree()
+    for id, original_record in original_records.items():
+        total_range = Interval(original_record.POS - 1, int(original_record.sv_end)) # Convert to 0-indexed half open
+        for alternate_record in alternate_records.get(id, []):
+            total_range = Interval(min(alternate_record.POS - 1, total_range.begin), max(int(alternate_record.sv_end), total_range.end))
+        variant_ranges.add(original_record.CHROM, total_range.begin, total_range.end, [id])
+
+    if args.merge_orig_blocks:    
+        variant_ranges.merge_overlaps(data_reducer=operator.add, data_initializer=[])
+
+
+    # Determine the best alternate representation(s) in each group
+    closest_alts = {}
+    for ids in variant_ranges.values():
+        if args.select == "dm2":
+            best_alts = [(float("Inf"), None, None)] * num_samples
+        elif args.select == "prob":
+            best_alts = [(0, None, None)] * num_samples
+        
+        for id in ids:
+            possible_alternate_records = alternate_records.get(id, [])
+            if args.include_orig_in_block:
+                possible_alternate_records.append(original_records[id])
+            for alternate_record in possible_alternate_records:
+                for i, alternate_call in enumerate(alternate_record.samples):
+                    best_score, *_ = best_alts[i]
+                    
+                    if args.select == "dm2" and alternate_call.data.DM:
+                        min_alt_dist_idx = np.argmin(alternate_call.data.DM)
+                        min_alt_dist = alternate_call.data.DM[min_alt_dist_idx]
+                        if min_alt_dist_idx == 0 or min_alt_dist >= best_score:
+                            continue
+                        best_alts[i] = (min_alt_dist, id, alternate_record)
+                    elif args.select == "prob" and alternate_call.data.AD:
+                        alt_prob = ad_to_prob(alternate_call.data.AD)
+                        max_alt_prob_idx = np.argmax(alt_prob)
+                        max_alt_prob = alt_prob[max_alt_prob_idx]
+                        if max_alt_prob_idx == 0 or max_alt_prob <= best_score:
+                            continue
+                        best_alts[i] = (max_alt_prob, id, alternate_record)
+                    else:
+                        continue
+
+        for id in ids:
+            closest_alts[id] = best_alts
+
+
     for id, record in original_records.items():
-        if id not in alternate_records:
+        if id not in closest_alts:
             # No alternate records present for this variant
             vcf_writer.write_record(record)
         else:
             # Update record with new FORMAT
             record.FORMAT = VCF_FORMAT
-
-            # Identify best alternate representation and genotype for each sample
+           
+            # Identify the best alternate representation and genotype for each sample
+            closest_alt = closest_alts[id]
             for i, call in enumerate(record.samples):
-                best_call = vcf.model._Call(
+                orig_call = vcf.model._Call(
                     record,
                     call.sample,
                     AltCallData(
                         GT=call.data.GT,
-                        PL=call.data.PL,
-                        DM=call.data.DM,
-                        AD=call.data.AD,
+                        PL=getattr(call.data, "PL", None),
+                        DM=getattr(call.data, "DM", None),
+                        AD=getattr(call.data, "AD", None),
                         CL=None,
                         OGT=None,
                         OPL=None,
@@ -238,48 +311,37 @@ def refine_variants(args, input_vcf: str, output_file):
                         OAD=None,
                     ),
                 )
-                if args.select == "dm2" and best_call.data.DM:
-                    min_dist = min(best_call.data.DM[1:])
+                record.samples[i] = orig_call
+                
+                alt_score, alt_id, alt_record = closest_alt[i]
+                if alt_record is None or alt_record is record:
+                    # No alternate record to update with, or we are tying to update with ourselves (same original record)
+                    continue
+                
+                if args.select == "dm2" and call.data.DM:
+                    orig_dist = min(call.data.DM[orig_start_idx:])
+                    if alt_score >= orig_dist:
+                        continue 
                 elif args.select == "prob" and best_call.data.AD:
-                    max_prob = max(ad_to_prob(best_call.data.AD)[1:])
+                    orig_prob = max(ad_to_prob(best_call.data.AD)[orig_start_idx:])
+                    if alt_score <= orig_dist:
+                        continue 
                 else:
-                    record.samples[i] = best_call
                     continue
 
-                # Identify other representations to see if we want to overwrite the genotype
-                # Update if:
-                # 1. Smallest DM^2 (largest GT prob) for alternate variant is for non-reference genotype, and
-                # 2. Alternate variant's smallest DM^2 (largest GT prob) is less (greater) than original's non-reference DM^2 (GT prob)
-                for alternate_record in alternate_records[id]:
-                    alternate_call = alternate_record.samples[i]
-                    if args.select == "dm2" and alternate_call.data.DM:
-                        min_alt_dist_idx = np.argmin(alternate_call.data.DM)
-                        min_alt_dist = alternate_call.data.DM[min_alt_dist_idx]
-                        if min_alt_dist_idx == 0 or min_alt_dist >= min_dist:
-                            continue
-                        min_dist = min_alt_dist
-                    elif args.select == "prob" and alternate_call.data.AD:
-                        alt_prob = ad_to_prob(alternate_call.data.AD)
-                        max_alt_prob_idx = np.argmax(alt_prob)
-                        max_alt_prob = alt_prob[max_alt_prob_idx]
-                        if max_alt_prob_idx == 0 or max_alt_prob <= max_prob:
-                            continue
-                        max_prob = max_alt_prob
-                    else:
-                        continue
-                    
-                    best_call_data = AltCallData(
-                        GT=alternate_call.data.GT,
-                        PL=alternate_call.data.PL,
-                        DM=alternate_call.data.DM,
-                        AD=alternate_call.data.AD,
-                        CL=variant_descriptor(alternate_record),
-                        OGT=call.data.GT,
-                        OPL=call.data.PL,
-                        ODM=call.data.DM,
-                        OAD=call.data.AD,
-                    )
-                    best_call = vcf.model._Call(record, call.sample, best_call_data)
-
+                alt_call = alt_record.samples[i]
+                best_call_data = AltCallData(
+                    GT=alt_call.data.GT if alt_id == id else "0/0",
+                    PL=getattr(alt_call.data, "PL", None),
+                    DM=getattr(alt_call.data, "DM", None),
+                    AD=getattr(alt_call.data, "AD", None),
+                    CL=variant_descriptor(alt_record),
+                    OGT=call.data.GT,
+                    OPL=getattr(call.data, "PL", None),
+                    ODM=getattr(call.data, "DM", None),
+                    OAD=getattr(call.data, "AD", None),
+                )
+                best_call = vcf.model._Call(record, call.sample, best_call_data)
                 record.samples[i] = best_call
+
             vcf_writer.write_record(record)
